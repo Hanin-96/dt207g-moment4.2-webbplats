063355e253f28547ae66bb05b1fd9c31b212b9dd
Hanin Farhan <haninfarhan96@gmail.com>
2024-05-14 13:35:38 +0200

    Lagt till user sida, css styling och funktioner f;r user vid inloggning

--- src/js/users.js
+++ src/js/users.js
@@ -1,15 +1,71 @@
 "use strict"
 
+
 window.onload = loadUsers;
 
 async function loadUsers() {
+
+    //Logga ut knapp
+    let btnSignOut = document.getElementById("btn-logout");
+    btnSignOut.addEventListener("click", signOut);
+
     let token = localStorage.getItem('token');
 
-    const response = await fetch("http://192.168.0.137:3000/user", {
+    const response = await fetch("https://dt207g-moment4-1.onrender.com/user", {
         headers: {
             'Authorization': 'Bearer ' + token
         }
     });
 
-    console.log(response);
+    //Ifall man är utloggad skickas man till startsidan
+    if (!response.ok) {
+        window.location.replace("/index.html");
+    } else {
+        //Vid lyckad inloggning skrivs datan ut på användarsidan
+        const data = await response.json();
+
+        console.log(data);
+
+        displayUsers(data.users);
+        loadImage();
+    }
+
+}
+
+//Skriv ut foto
+import userImage from '../img/user.png';
+
+function loadImage() {
+    let imageWrap = document.getElementById("image-user");
+    let imageEl = document.createElement("img");
+    imageEl.src = userImage;
+    imageEl.alt = "user icon";
+
+    imageWrap.appendChild(imageEl);
+}
+
+//Skriva ut värden på Användarsidan
+function displayUsers(users) {
+
+    //Skriva ut användare från localStorage
+    let userId = document.getElementById("usernameId");
+    let usernameSignedIn = localStorage.getItem('username');
+
+    userId.innerHTML = usernameSignedIn;
+
+
+    let userWrap = document.querySelector(".users-wrap");
+    userWrap.innerHTML = "";
+
+    users.forEach(user => {
+        userWrap.innerHTML += `<p>${user.username + ": " + user.firstname + " " + user.lastname}</p>`
+
+    });
+
+}
+
+//Funktion för att logga ut & rensa token i localStorage
+function signOut() {
+    localStorage.removeItem("token");
+    window.location.replace("/index.html")
 }
